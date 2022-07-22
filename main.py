3fdb9f8a75e6d8e0ed36765cdd2f125e6d32107b
nahamta <haniyehshahari@gmail.com>
2022-07-22 15:21:47 +0430

    all file

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 # driver = webdriver.Chrome(executable_path='/home/nima/Working/Selenium/chromedriver')
 driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
-driver.get("http://google.com")
+driver.get("https://google.com")
